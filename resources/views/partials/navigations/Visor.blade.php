d6d1551590dbb6de20dc6a854505ebe635779db1
Genaro Perez <genaromajestice@gmail.com>
2023-09-07 10:50:22 -0600

    Update Visor.blade.php

--- resources/views/partials/navigations/Visor.blade.php
+++ resources/views/partials/navigations/Visor.blade.php
@@ -7,5 +7,8 @@
 <li>
 	<a class="nav-link" href="{{ route('information.clientes.lista') }}">{{ __("Clientes") }}</a>
 </li>
+<li>
+	<a class="nav-link" href="{{ route('libro.all_sistemas') }}">{{ __("Inventario") }}</a>
+</li>
 <user-notifications :user_id="{{auth()->user()->id}}" :noleidos="{{Auth::user()->unreadNotifications}}"></user-notifications>
 @include('partials.navigations.logged')
